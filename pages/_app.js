c60aeb32ee699c73be99873e4a7924586cb6b1bf
JM-M <mikejolamoses@gmail.com>
2024-02-23 15:09:20 +0100

    Added orb context

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,41 @@
-
-import '../styles/globals.css'
-import "../styles/survey.css"
-import { Orbis, OrbisProvider } from "@orbisclub/components";
-import "@orbisclub/components/dist/index.modern.css";
+import '../styles/globals.css';
+import '../styles/survey.css';
+import { Orbis, OrbisProvider } from '@orbisclub/components';
+import '@orbisclub/components/dist/index.modern.css';
 import React from 'react';
-import { GlobalContext } from "../contexts/GlobalContext";
+import { GlobalContext } from '../contexts/GlobalContext';
 
 /**
  * Set the global forum context here (you can create categories using the dashboard by clicking on "Create a sub-context"
  * from your main forum context)
  */
-global.orbis_context = "";
+global.orbis_context =
+  'kjzl6cwe1jw148fqdrvokzob6yzu9cw22oglkxxnkr0pj39pmzy8eiyzmq5mzr9';
 
 /**
  * Set the global chat context here (the chat displayed when users click on the "Community Chat" button).
  * The Community Chat button will be displayed only if this variable is set
  */
-global.orbis_chat_context = "";
+global.orbis_chat_context =
+  'kjzl6cwe1jw148fqdrvokzob6yzu9cw22oglkxxnkr0pj39pmzy8eiyzmq5mzr9';
 
 let orbis = new Orbis({
   useLit: true,
-  node: "https://node2.orbis.club",
+  node: 'https://node2.orbis.club',
   PINATA_GATEWAY: 'https://orbis.mypinata.cloud/ipfs/',
   PINATA_API_KEY: process.env.NEXT_PUBLIC_PINATA_API_KEY,
-  PINATA_SECRET_API_KEY: process.env.NEXT_PUBLIC_PINATA_SECRET_API_KEY
+  PINATA_SECRET_API_KEY: process.env.NEXT_PUBLIC_PINATA_SECRET_API_KEY,
 });
 
 export default function App({ Component, pageProps }) {
-  return(
-    <OrbisProvider defaultOrbis={orbis} authMethods={["metamask", "wallet-connect", "email"]}>
-      <GlobalContext.Provider value={{orbis: orbis}}>
+  return (
+    <OrbisProvider
+      defaultOrbis={orbis}
+      authMethods={['metamask', 'wallet-connect', 'email']}
+    >
+      <GlobalContext.Provider value={{ orbis: orbis }}>
         <Component {...pageProps} />
       </GlobalContext.Provider>
     </OrbisProvider>
-  )
+  );
 }
